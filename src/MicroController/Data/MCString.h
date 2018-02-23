9c80e5f53320228e1b44d97f38a84f71c4273537
Taylor Gilbert <taylorgilbert@Taylors-MacBook-Pro.local>
2018-02-22 18:35:24 -0600

    Added header to MCString.h

---
+++ src/MicroController/Data/MCString.h
@@ -0,0 +1,12 @@
+//
+//  MCString.h
+//  MicroController
+//
+//  Created on 2/16/18.
+//  Copyright (c) 2018 UAH Electric Vehicle Club
+//  Licensed under Apache License v2.0
+//
+//  See README.md and LISCENSE for more details
+//
+//  Defines the base data type for MicroController. All
+//  other data types inherit from MCData or its children
