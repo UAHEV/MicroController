1f3dc1126fa0f93ad3192a2f052da71fd6afcb66
areichle <areichle76@me.com>
2018-02-22 18:33:01 -0600

    Added comment

---
+++ src/MicroController/Device/MCInputDevice.h
@@ -0,0 +1,12 @@
+//
+//  MCInputDevice.h
+//  MicroController
+//
+//  Created on 2/22/18.
+//  Copyright (c) 2018 UAH Electric Vehicle Club
+//  Licensed under Apache License v2.0
+//
+//  See README.md and LISCENSE for more details
+//
+//  Defines the base class for a MicroController InputDevice. All
+//  other input devices will inherit from this class.
