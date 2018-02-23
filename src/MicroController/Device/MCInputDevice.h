be0dcc6933496d018c4aabe1c0614969f3156f94
areichle <areichle76@me.com>
2018-02-22 19:42:37 -0600

    Finished header

--- src/MicroController/Device/MCInputDevice.h
+++ src/MicroController/Device/MCInputDevice.h
@@ -10,4 +10,20 @@
 //
 //  Defines the base class for a MicroController InputDevice. All
 //  other input devices will inherit from this class.
-//
+
+#ifndef MICROCONTROLLER_DEVICE_MCINPUTDEVICE_H_
+#define MICROCONTROLLER_DEVICE_MCINPUTDEVICE_H_
+#include "MCDevice.h"
+
+class MCInputDevice: MCDevice {
+ public:
+
+  // The default constructor
+  MCInputDevice();
+
+  // The virtual function for returning data. This is required to be implemented.
+  virtual void get_next() =0;
+
+};
+
+#endif // MICROCONTROLLER_DEVICE_MCINPUTDEVICE_H_
