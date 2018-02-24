38646c228f4145c2f5dba750df9c1dcbdeb3a1b7
willboyd8 <will.boyd@abwlabs.com>
2018-02-24 17:51:23 -0600

    Added MCData and MCInteger includes

---
+++ src/MicroController/MicroController.h
@@ -0,0 +1,19 @@
+//
+//  MicroController.h
+//  MicroController
+//
+//  Created on 2/17/18.
+//  Copyright (c) 2018 UAH Electric Vehicle Club
+//  Licensed under Apache License v2.0
+//
+//  See README.md and LISCENSE for more details
+//
+//  A framework for Master-Slave based control systems
+
+#ifndef MICROCONTROLLER_MICROCONTROLLER_H_
+#define MICROCONTROLLER_MICROCONTROLLER_H_
+
+#include "Data/MCData.h"
+#include "Data/MCInteger.h"
+
+#endif // MICROCONTROLLER_MICROCONTROLLER_H_
