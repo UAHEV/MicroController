a13a776f9abd43db4ac956b4a60ee7fae3708b97
willboyd8 <will.boyd@abwlabs.com>
2018-02-26 07:57:22 -0600

    Added Initial MCDevice stub

---
+++ src/MicroController/Device/MCDevice.h
@@ -0,0 +1,27 @@
+//
+//  MCDevice.h
+//  MicroController
+//
+//  Created on 2/17/18.
+//  Copyright (c) 2018 UAH Electric Vehicle Club
+//  Licensed under Apache License v2.0
+//
+//  See README.md and LISCENSE for more details
+//
+//  A framework for Master-Slave based control systems
+
+#ifndef MICROCONTROLLER_DEVICE_MCDEVICE_H_
+#define MICROCONTROLLER_DEVICE_MCDEVICE_H_
+#include "MCConnection.h"
+
+
+class MCDevice {
+ public:
+
+ private:
+  MCConnection connection;
+
+
+};
+
+#endif // MICROCONTROLLER_DEVICE_MCDEVICE_H_
